726f18e9790b98c2442878d40f16b12d834bc0fd
Tushar <kmrtushar01@gmail.com>
2024-03-28 13:00:08 +0530

    adding jsx and testimonial component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import Testimonials from "./componenets/Testimonials"
+import reviews from "./data";
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+
+        <div>
+          <h1>Our Testimonials</h1>
+          <div></div>
+        </div>
+        <Testimonials reviews={reviews}/>
+      </div>
     </div>
   );
 }
