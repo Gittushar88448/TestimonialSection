dc3c356279528b1db8842c5b34f61f5656a2a776
Tushar <kmrtushar01@gmail.com>
2024-03-28 13:35:54 +0530

    adding tailwind css in testimonial sec

--- src/componenets/Testimonials.js
+++ src/componenets/Testimonials.js
@@ -5,21 +5,23 @@ import React from 'react'
 const Testimonials = (props) => {
     let reviews = props.reviews[0];
   return (
-    <div>
+    <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center
+    mt-10 p-10 transition-all duration-700 hover:shadow-xl rounded-md'>
         <Card reviews={reviews}></Card>
 
-        <div>
-            <button>
+        <div className='flex text-3xl mt-10 gap-3 text-violet-400 font-bold mx-auto'>
+            <button className='cursor-pointer hover:text-violet-500 '>
                 <FiChevronLeft/>
             </button>
 
-            <button>
+            <button className='cursor-pointer hover:text-violet-500 '>
                 <FiChevronRight/>
             </button>
         </div>
 
         <div>
-            <button>
+            <button className='bg-violet-400 hover:bg-violet-500 transition-all duration-200 mt-6
+        cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg'>
                 Surprise Me
             </button>
         </div>
